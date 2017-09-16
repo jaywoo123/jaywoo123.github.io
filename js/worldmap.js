c122848f95eb00a4151075cf768cc3e5288b03a0
Jae Woo Kim <jaywoo123@hotmail.com>
2017-09-15 19:21:04 -0500

    almost done

--- js/worldmap.js
+++ js/worldmap.js
@@ -41,15 +41,14 @@ var zoom = new Datamap({
   }
 });
 zoom.bubbles([
- {name: 'Gabon was a uniqe experience', latitude: 0.8037, longitude: 11.6094, radius: 8, fillKey: 'gt500'},
- {name: 'Bubble 2', latitude: 35.9078, longitude: 127.7669, radius: 8, fillKey: 'eq0'},
- {name: 'Bubble 3', latitude: 61.5240, longitude: 105.3188, radius: 8, fillKey: 'lt25'},
- {name: 'Bubble 4', latitude: 15.8700, longitude: 100.9925, radius: 8, fillKey: 'eq50'},
- {name: 'Bubble 5', latitude: 38.89511, longitude: -77.03637, radius: 8, fillKey: 'eq50'},
- {name: 'Bubble 6', latitude: 42.3601, longitude: -71.0589, radius: 8, fillKey: 'eq50'},
- {name: 'Bubble 7', latitude: 15.8700, longitude: 100.9925, radius: 8, fillKey: 'eq50'},
- {name: 'Bubble 8', latitude: 34.0522, longitude: -118.2437, radius: 8, fillKey: 'eq50'},
- {name: 'Bubble 8', latitude: 40.116421, longitude:  -88.243385, radius: 8, fillKey: 'eq50'},
+ {name: 'Gabon was the most exotic place I visited.', latitude: 0.8037, longitude: 11.6094, radius: 8, fillKey: 'gt500'},
+ {name: 'Although I have been abroad most of my life, my roots originate from South Korea. I love visiting SK frequently to spend time with my family.', latitude: 35.9078, longitude: 127.7669, radius: 8, fillKey: 'eq0'},
+ {name: 'Russia was the first country I moved to when I was an adolescent. I spoke Russian fluently until coming of age. I still remember the cold nights of moscow.', latitude: 61.5240, longitude: 105.3188, radius: 8, fillKey: 'lt25'},
+ {name: 'I went to garde school in DC. I picked up English as my second language.', latitude: 15.8700, longitude: 100.9925, radius: 8, fillKey: 'eq50'},
+ {name: 'I went to garde school in DC. I picked up English as my second language.', latitude: 38.89511, longitude: -77.03637, radius: 8, fillKey: 'eq50'},
+ {name: 'I attended the last two years of my highschool in Thailand. Thailand opened my eyes to many different social contexts. I met people of royal blood to those who live in the slums. I decided to persue engineering after enlightening moments in Thailand.', latitude: 15.8700, longitude: 100.9925, radius: 8, fillKey: 'eq50'},
+ {name: 'I started highschool in Beverly Hills. Yes, I saw some hollywood movie stars and went over to one of their houses.', latitude: 34.0522, longitude: -118.2437, radius: 8, fillKey: 'eq50'},
+ {name: 'I went to middle school in Boston. One of my favorite foods became Boston Clam Chowder. Clam Chowder is better in Boston because there is always piles of snow.', latitude: 40.116421, longitude:  -88.243385, radius: 8, fillKey: 'eq50'},
 ], {
  popupTemplate: function(geo, data) {
    return "<div class='hoverinfo'>" + data.name + "";
